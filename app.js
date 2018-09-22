7e98fdd7666af058845969620668e2374797c3f7
Gordo <cortland.taylor1@gmail.com>
2018-09-21 22:10:49 -0700

    sdjfhgsdjfgh

---
+++ app.js
@@ -0,0 +1,15 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBMBk2DA7dF1KlGCDUu_bYr4BDfk0mSLVE",
+    authDomain: "train-scheduler-ef249.firebaseapp.com",
+    databaseURL: "https://train-scheduler-ef249.firebaseio.com",
+    projectId: "train-scheduler-ef249",
+    storageBucket: "train-scheduler-ef249.appspot.com",
+    messagingSenderId: "377154641362"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
